ac70fc87d82f4019634d70266882de32776089aa
Jens Vadim Heim <jensvadim@hotmail.com>
2025-06-12 20:12:38 +0200

    rename css classes for clarity

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,15 +1,19 @@
 export default function WeatherInfo({ data }) {
   return (
     <div className="weather-info">
-      <div className="main-info">
-        <div className="left">
-          <p className="city">
+      <div className="weather-main">
+        <div className="weather-main-left">
+          <p className="weather-location">
             {data.name}, {data.sys.country}
           </p>
-          <p className="temp">{data.main.temp}℃</p>
+          <p className="weather-temp">{data.main.temp}℃</p>
+          <div className="weather-range">
+            <p className="weather-low">L {data.main.temp_min}℃</p>
+            <p className="weather-high">H {data.main.temp_max}℃</p>
+          </div>
         </div>
 
-        <div className="right">
+        <div className="weather-main-right">
           <img
             src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             alt="Weather icon"
@@ -18,22 +22,23 @@ export default function WeatherInfo({ data }) {
         </div>
       </div>
 
-      <div className="weather-cards">
-        <div className="info-card">
-          <p className="text">Feels like</p>{" "}
-          <p className="data">{data.main.feels_like}℃</p>
+      <hr />
+      <div className="weather-details">
+        <div className="weather-card">
+          <p className="weather-card-label">Feels like</p>{" "}
+          <p className="weather-card-value">{data.main.feels_like}℃</p>
         </div>
-        <div className="info-card">
-          <p className="text">Humidity</p>{" "}
-          <p className="data">{data.main.humidity}</p>
+        <div className="weather-card">
+          <p className="weather-card-label">Humidity</p>{" "}
+          <p className="weather-card-value">{data.main.humidity}</p>
         </div>
-        <div className="info-card">
-          <p className="text">Wind speed</p>{" "}
-          <p className="data">{data.wind.speed}</p>
+        <div className="weather-card">
+          <p className="weather-card-label">Wind speed</p>{" "}
+          <p className="weather-card-value">{data.wind.speed}</p>
         </div>
-        <div className="info-card">
-          <p className="text">Pressure</p>{" "}
-          <p className="data">{data.main.pressure}</p>
+        <div className="weather-card">
+          <p className="weather-card-label">Pressure</p>{" "}
+          <p className="weather-card-value">{data.main.pressure}</p>
         </div>
       </div>
     </div>
